1227c69282cae18a822d3a541ef2d2f3d707833a
poom029 <poom_22love@hotmail.com>
2017-01-02 20:41:33 +0700

    random positions + bot aim player

--- js/code.js
+++ js/code.js
@@ -21,8 +21,8 @@ var cursors;
 var fireButton;
 //var bot;
 var timer,total=0;
-var randPositionX;
-var randPositionY;
+var randPositionX=[];
+var randPositionY=[];
 var bullets;
 var fireRate = 100;
 var nextFire = 0;
@@ -94,7 +94,10 @@ function create() {
 
     sprite.body.collideWorldBounds = true;
     enemy = [];
-    enemy.push(new EnemyShip(0, game, enemyBullets));
+    for (var i = 0; i < 3; i++){
+        enemy.push(new EnemyShip(i, game, enemyBullets));
+    }
+
 
     timer = game.time.create(false);
     //  Set a TimerEvent to occur after 2 seconds
@@ -105,28 +108,34 @@ function create() {
 }
 
 function update() {
-	game.physics.arcade.overlap(enemy[0].ship,bullets, bulletHitEnemy, null , this);
-    game.physics.arcade.overlap(sprite,enemyBullets, bulletHitPlayer, null , this);
-    sprite.body.velocity.y=0;
-	sprite.body.velocity.x=0;
-	if(cursors.up.isDown){
-		sprite.body.velocity.y = -300;
-	}
-	if(cursors.down.isDown){
-		sprite.body.velocity.y = 300;
-	}
-	if(cursors.left.isDown){
-		sprite.body.velocity.x = -300;
-	}
-	if(cursors.right.isDown){
-		sprite.body.velocity.x = 300;
-	}
-
     if (fireButton.isDown)
     {
         fire();
     }
-    enemy[0].update();
+    game.physics.arcade.overlap(sprite,enemyBullets, bulletHitPlayer, null , this);
+    for (var i = 0; i < 3; i++){
+        game.physics.arcade.overlap(enemy[i].ship,bullets, bulletHitEnemy, null , this);
+        if(enemy[i].alive){
+          enemy[i].update(i);
+        }
+    }
+    sprite.body.velocity.y=0;
+	  sprite.body.velocity.x=0;
+  	if(cursors.up.isDown){
+  		sprite.body.velocity.y = -300;
+  	}
+  	if(cursors.down.isDown){
+  		sprite.body.velocity.y = 300;
+  	}
+  	if(cursors.left.isDown){
+  		sprite.body.velocity.x = -300;
+  	}
+  	if(cursors.right.isDown){
+  		sprite.body.velocity.x = 300;
+  	}
+
+
+
 
     //game.world.wrap(sprite, 16);
     //game.world.wrap(bot, 16);
@@ -180,23 +189,25 @@ EnemyShip.prototype.damage = function() {
 
 }
 function reposition() {
-    randPositionX = game.rnd.integerInRange(1, 799);
-    randPositionY = game.rnd.integerInRange(1, 300);
-    total++;
+    for (var i = 0; i < 3; i++){
+      randPositionX[i] = game.rnd.integerInRange(1, 799);
+      randPositionY[i] = game.rnd.integerInRange(1, 300);
+    }
+    //total++;
 }
-EnemyShip.prototype.update = function() {
+EnemyShip.prototype.update = function(i) {
     this.ship.body.velocity.y=0;
     this.ship.body.velocity.x=0;
-    if(this.ship.x - randPositionX <= 10){
+    if(this.ship.x - randPositionX[i] <= 10){
         this.ship.body.velocity.x = 150;
     }else
-    if(this.ship.x - randPositionX >= 20){
+    if(this.ship.x - randPositionX[i] >= 20){
         this.ship.body.velocity.x = -150;
     }
-    if(this.ship.y - randPositionY <= 10){
+    if(this.ship.y - randPositionY[i] <= 10){
         this.ship.body.velocity.y = 150;
     }else
-    if(this.ship.y - randPositionY >= 20){
+    if(this.ship.y - randPositionY[i] >= 20){
         this.ship.body.velocity.y = -150;
     }
     if(this.alive)
@@ -246,6 +257,7 @@ function fireBot (ship) {
             bullet.reset(ship.x-30, ship.y-20);
             bullet.body.velocity.y = 200;
             bulletTime = game.time.now + 500;
+            bullet.rotation = this.game.physics.arcade.moveToObject(bullet, sprite, 350);
         }
     }
 }
